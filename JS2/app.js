c712f33959447a9de1e2b891e85c24919dbac207
Alexinerror <sarmat674@gmail.com>
2022-12-11 20:09:31 +0400

    task2 and task3 added

---
+++ JS2/app.js
@@ -0,0 +1,11 @@
+let x = prompt('enter year of birth');
+let y = 2022;
+let result = y - x;
+console.log(result);
+alert (result)
+
+let a = prompt('enter value in km')
+let b = 0.6214;
+let res = a * b;
+console.log(res);
+alert (res)
