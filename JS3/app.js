35e3b72d0011e32b27b9a1ae189161f65b4f05e8
Alexinerror <sarmat674@gmail.com>
2022-12-15 21:50:46 +0400

    task 3 added

---
+++ JS3/app.js
@@ -0,0 +1,17 @@
+let x = prompt('enter your name');
+let y = prompt('enter your surname');
+let result = x + ' ' + y;
+console.log(result);
+alert (result);
+
+let a = prompt('enter your name');
+let b = '-->'
+let res = a + ' ' + b
+console.log(res);
+alert (res);
+
+let c = prompt('enter number');
+let d = prompt('enter number');
+let rs = c*2 + d*2;
+console.log(rs);
+alert (rs);
